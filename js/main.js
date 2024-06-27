78f43873660ce8630a889d96b3026ef30d854a12
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-06-27 15:35:59 -0500

    feat :sparkles: se crea la seccion de descripción
    se creo las descripciones procedere al titulo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { displayRockets } from "./module/rockets.js";
 
 console.log( await displayRockets())
+console.log(await displayRockets1())
--- js/module/rockets.js
+++ js/module/rockets.js
@@ -7,6 +7,10 @@ export const displayRockets = async()=>{
 
     let namme = rocket.description;
     let country = rocket.country;
+    let price = rocket.cost_per_launch;
+    let fecha =rocket.first_flight;
+    let wilki = rocket.wikipedia;
+    
     let plantilla = `
     <div class="continformation">
                     <div class="conttexto">
@@ -17,29 +21,36 @@ export const displayRockets = async()=>{
                         <h1>${namme}</h1>
                     </div>
                 </div>
-    `;
-    contenedor.innerHTML= plantilla;
-    
-}
-
-export const displayRockets1 = async()=>{
-    let contenedor = document.querySelector("#contsection1")
-    let rockets = await getAllRockets();
-    let rocket = rockets[0];
-
-    let namme = rocket.description;
-    let country = rocket.country;
-    let plantilla = `
-    <div class="continformation">
+                <div class="continformation">
                     <div class="conttexto">
                         <img src="storage/image/mech.svg" alt="">
                     </div>
                     <div class="conttexto">
-                        <h2>${country}</h2>
-                        <h1>${namme}</h1>
+                        <h2>The estimated cost per rocket launch</h2>
+                        <h1>$ ${price}</h1>
+                    </div>
+                </div>
+                </div>
+                <div class="continformation">
+                    <div class="conttexto">
+                        <img src="storage/image/mech.svg" alt="">
+                    </div>
+                    <div class="conttexto">
+                        <h2>The date of the first flight of the rocket</h2>
+                        <h1>${fecha}</h1>
+                    </div>
+                </div>
+                 </div>
+                <div class="continformation">
+                    <div class="conttexto">
+                        <img src="storage/image/mech.svg" alt="">
+                    </div>
+                    <div class="conttexto">
+                        <h2>Read more about the coete</h2>
+                        <h1>${wilki}</h1>
                     </div>
                 </div>
     `;
-    contenedor.innerHTML= plantilla;
     
+    contenedor.innerHTML= plantilla;
 }
