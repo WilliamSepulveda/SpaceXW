1f3020fed5c80e3a4415a0fff6fee683924f1f98
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-06-27 20:21:34 -0500

    feat :sparkles: se crea el contenedor de informacion del cohete
    se creo el modulo de info del cohete con la data de la API usando estilos para mi diseño personal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { displayRockets } from "./module/rockets.js";
 import { titleRockets } from "./module/rockets.js";
+import { informationRocket } from "./module/rockets.js";
 
+
+console.log (await informationRocket())
 console.log( await displayRockets())
 console.log(await titleRockets())
 
--- js/module/rockets.js
+++ js/module/rockets.js
@@ -67,3 +67,51 @@ export const titleRockets = async()=>{
     `;
     contenedor.innerHTML = plantilla;
 }
+
+
+export const informationRocket = async()=>{
+    let contenedor = document.querySelector("#informationR")
+    let rockets = await getAllRockets();
+    let rocket = rockets[0];
+
+    let typeRocket = rocket.type;
+    let etapa = rocket.stages;
+    let estado = rocket.active;
+    let propulsores = rocket.boosters;
+    let Landing_legs = rocket.landing_legs.number;
+    let material = rocket.landing_legs.material;
+    if (rocket.landing_legs.material !== null) {
+        material = rocket.landing_legs.material;
+    } else {
+        material = '';
+    }
+    let plantilla =`
+        <div class="cont_espera">
+                    <h1></h1>
+                </div>  
+                <div class="content_information">
+                    <div class="titlecontent_information">
+                        <h1>INFORMATION ROCKET<br>••••••••••••••••••••••••••••</h1> 
+                    </div>
+                    <div class="contenido">
+                        <div class="contenido_hijo">
+                        <p>Type</p>
+                        <p>Rocket in service</p>
+                        <p>Number of stages</p>
+                        <p>Number of propellants</p>
+                        <p>Landing legs</p>
+                        <p>Leg material</p>
+                        </div>
+                        <div class="contenido_hijo2">
+                            <p>${typeRocket}</p>
+                            <p>${estado}</p>    
+                            <p>${etapa}</p>
+                            <p>${propulsores}</p>
+                            <p>${Landing_legs}</p>
+                            <p>${material}</p>
+                        </div>
+                    </div>  
+                </div>
+    `;
+    contenedor.innerHTML = plantilla;
+}
